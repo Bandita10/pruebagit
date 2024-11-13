f607d269037849abe94e8f6780002dac766f314a
Bandita10 <angelbandaj@outlook.com>
2024-11-13 15:39:41 -0600

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,18 @@
+package logica;
+
+import igu.Pantalla;
+
+public class Calculadora {
+
+  
+    public static void main(String[] args) {
+       
+    Pantalla panta = new Pantalla();
+    panta.setVisible(true);
+    panta.setLocationRelativeTo(null);
+    
+    
+    
+    }
+
+}
